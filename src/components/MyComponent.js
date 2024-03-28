497c37ca42969b347569ef40e086982722814ec3
Phan Thanh Tri <tript2609@gmail.com>
2024-03-29 03:00:34 +0700

    MyFirstComponent

---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+// class component
+// function component
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Phan Thanh Trí with React JS</h1>
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
