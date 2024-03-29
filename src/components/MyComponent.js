76608c9e789feafa0fb88cb621b4088e6c0cf950
Phan Thanh Tri <tript2609@gmail.com>
2024-03-29 23:19:21 +0700

    State-Data of ReactApp

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,11 +3,18 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "PT26",
+    age: 21,
+    address: "Trà Vinh",
+  };
   render() {
     return (
       <div>
         <h1>Phan Thanh Trí with React JS</h1>
-        {Math.random()}
+        <p>
+          Tuổi: {this.state.age}, Địa chỉ: {this.state.address}
+        </p>
       </div>
     );
   }
