ce8826f52df854644d81b3015327eaef9eebdd39
Phan Thanh Tri <tript2609@gmail.com>
2024-03-30 00:32:02 +0700

    DOM EVENT

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,20 +1,33 @@
 // class component
 // function component
 import React from "react";
-
+import ReactDOM from "react-dom";
 class MyComponent extends React.Component {
   state = {
     name: "PT26",
     age: 21,
     address: "Trà Vinh",
   };
+
+  handleClick = (event) => {
+    console.log("Click me");
+    console.log(event);
+  };
+
+  handleOnMouseOver = (event) => {
+    console.log(event);
+  };
+
   render() {
     return (
-      <div>
+      <div id="root">
         <h1>Phan Thanh Trí with React JS</h1>
         <p>
-          Tuổi: {this.state.age}, Địa chỉ: {this.state.address}
+          Tên: {this.state.name}, Tuổi: {this.state.age}, Địa chỉ:{" "}
+          {this.state.address}
         </p>
+        <button onMouseOver={this.handleOnMouseOver}>Click me</button>
+        <button onClick={this.handleClick}>Click me</button>
       </div>
     );
   }
