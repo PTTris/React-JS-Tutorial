af17bd51995a38ea89af1b7cd6efd2c93c2f11d5
Phan Thanh Tri <tript2609@gmail.com>
2024-03-30 16:16:11 +0700

    Handle Form Basic

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,34 +3,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 class MyComponent extends React.Component {
-  state = {
-    name: "PT26",
-    age: 21,
-    address: "Trà Vinh",
-  };
+   state = {
+      name: "PT26",
+      age: 21,
+      address: "Trà Vinh",
+   };
 
-  handleClick = (event) => {
-    console.log("Click me");
-    console.log(event);
-  };
+   handleClick = (event) => {
+      console.log("Click me");
+      this.setState({
+         name: "TuKhanh",
+         age: 18,
+      });
+   };
 
-  handleOnMouseOver = (event) => {
-    console.log(event);
-  };
+   handleOnMouseOver = (event) => {
+      console.log(event);
+   };
 
-  render() {
-    return (
-      <div id="root">
-        <h1>Phan Thanh Trí with React JS</h1>
-        <p>
-          Tên: {this.state.name}, Tuổi: {this.state.age}, Địa chỉ:{" "}
-          {this.state.address}
-        </p>
-        <button onMouseOver={this.handleOnMouseOver}>Click me</button>
-        <button onClick={this.handleClick}>Click me</button>
-      </div>
-    );
-  }
+   handleOnchange = (event) => {
+      this.setState({ name: event.target.value });
+   };
+
+   handleOnsubmit = (event) => {
+      event.preventDefault();
+      console.log(this.state);
+   }
+
+   render() {
+      return (
+         <div>
+         <h1>PT26 with ReactJS</h1>
+         <p>My name: {this.state.name}</p>
+         <form onSubmit={(event) => this.handleOnsubmit(event)}>
+            <input type="text" onChange={(event) => this.handleOnchange(event)} />
+            <button>Submit</button>
+         </form>
+         </div>
+      );
+   }
 }
 
 export default MyComponent;
--- src/components/TuKhanh.js
+++ src/components/TuKhanh.js
@@ -1,6 +1,6 @@
 import React from "react";
 export default class TuKhanh extends React.Component {
-  render() {
-    return <p>Trần Tú Khanh 18/02/2006</p>;
-  }
+     render() {
+     return <p>Trần Tú Khanh 18/02/2006</p>;
+}
 }
